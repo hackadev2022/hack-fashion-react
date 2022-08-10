2ac0c86d54a9d823c94f986e0c41e52952a095ac
Daniel Lucas <daniellucas.bio@gmail.com>
2022-08-09 22:03:35 -0300

    add changes to global variables and App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+
+
 
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+
   return (
     <>
-      HELLO MANS
+      <button onClick={increment}> Click ME! </button>
+      <h1>{count}</h1>
+
+      <br />
+
     </>
   );
 }
