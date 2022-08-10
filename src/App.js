c1d97173bc1d4970b0ec0bd8a2ee9b684983eb61
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-10 15:56:57 -0300

    alterei o index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
 import { Header } from "./components/Header/Header";
+import { Banner } from "./components/Banner/Banner";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner
+        bannerTitle="Título do banner"
+        bannerMainInfo="10% off"
+        bannerInfo="Não perca essa oportunidade !"
+        bannerDirectoryImg="banners/banner_ilustrativo3.png"
+      />
     </>
   );
 }
