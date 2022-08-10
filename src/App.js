5f314432d764fd979c8b99640cd3bb2621444154
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-10 10:34:04 -0300

    add Header no componente App | add index.css | add mais colors no global.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-
-
-
-
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount(count + 1);
-
   return (
     <>
-      <button onClick={increment}> Click ME! </button>
-      <h1>{count}</h1>
-
-      <br />
-
+      <Header />
     </>
   );
 }
