60cd8ba61e243d00c164b0add1e47fa57b0f214f
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-10 10:14:57 -0300

    add component MenuNavBar

---
+++ src/components/MenuNavBar/MenuNavBar.jsx
@@ -0,0 +1,21 @@
+import "./MenuNavBar.css";
+
+export const MenuNavBar = ({ menuWidth, closeMenu }) => {
+  setTimeout(() => {
+    let divMenuNavBar = document.querySelector(".menu-nav-bar");
+    divMenuNavBar.style.width = menuWidth;
+  }, 10);
+
+  return (
+    <div className="menu-nav-bar">
+      <i className="menu fa-solid fa-xmark" onClick={closeMenu}></i>
+      <ul>
+        <li onClick={closeMenu}>Jeans</li>
+        <li onClick={closeMenu}>Camisas</li>
+        <li onClick={closeMenu}>Suéter</li>
+        <li onClick={closeMenu}>Calças moletom</li>
+        <li onClick={closeMenu}>Camisas moletom</li>
+      </ul>
+    </div>
+  );
+};
