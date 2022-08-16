aca1aae0b2363a5fab5bcd823bad79d04e8e9c16
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-16 10:16:51 -0300

    Alterações para boas práticas

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/cadastro";
-import { Footer } from "./components/Footer/Footer";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
 
-          <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
 
-          <Route path="/Cadastro" element={<Cadastro />}></Route>
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
+        <Route path="/Cadastro" element={<Cadastro />}></Route>
+      </Routes>
     </>
   );
 }
