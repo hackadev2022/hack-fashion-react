5fb606be1c96cbd8ef9daa10873d6f274ab2b72f
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-16 09:47:33 -0300

    criei a page home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Banner } from "../../components/Banner/Banner";
+
+export const Home = () => {
+  return (
+    <Banner
+      bannerTitle="Título do banner"
+      bannerMainInfo="10% off"
+      bannerInfo="Não perca essa oportunidade !"
+      bannerDirectoryImg="banners/banner_ilustrativo3.png"
+    />
+  );
+};
