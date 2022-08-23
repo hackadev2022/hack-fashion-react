964acf601f6a0cde80fbcb1a6c17170be8167a3f
Geovanna <geovannacunha31@gmail.com>
2022-08-23 02:05:22 -0300

    Slider + Categories

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./Categories.css";
+import { Navigation } from "swiper";
+
+export const Categories = () => {
+  return (
+    <>
+      <h2 className="title-categories">Categorias</h2>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={35}
+        slidesPerGroup={1}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={false}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide >
+          <img src="/img/roupas_sem_fundo/jeans1.png" />
+          <a href="#" className="name-categories">Jeans</a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/roupas_sem_fundo/shirt2.png" />
+          <a href="#" className="name-categories">Camisas</a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/roupas_sem_fundo/sweater2.png"/>
+          <a href="#" className="name-categories">Suéter</a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/roupas_sem_fundo/sweatpants1.png" />
+          <a href="#" className="name-categories">Calças Moletom</a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/roupas_sem_fundo/sweatshirt3.png" />
+          <a href="#" className="name-categories">Camisas Moletom</a>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
