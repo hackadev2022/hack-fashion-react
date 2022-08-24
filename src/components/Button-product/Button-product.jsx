c675c8f59d7c0701b09aca3440a15274357a3ce9
Geovanna <geovannacunha31@gmail.com>
2022-08-24 20:30:55 -0300

    Created folder for slider + better css and html

--- src/components/Button-product/Button-product.jsx
+++ src/components/Button-product/Button-product.jsx
@@ -8,36 +8,16 @@ export const ButtonProduct = () => {
     <>
       <div className="input-size">
         <p className="identifier">Tamanho:</p>
-        <input
-          type="radio"
-          id="P"
-          name="size"
-          value="P"
-        />
+        <input type="radio" id="P" name="size" value="P" />
         <label for="P">P</label>
 
-        <input
-          type="radio"
-          id="M"
-          name="size"
-          value="M"
-        />
+        <input type="radio" id="M" name="size" value="M" />
         <label for="M">M</label>
 
-        <input
-          type="radio"
-          id="G"
-          name="size"
-          value="G"
-        />
+        <input type="radio" id="G" name="size" value="G" />
         <label for="G">G</label>
 
-        <input
-          type="radio"
-          id="GG"
-          name="size"
-          value="GG"
-        />
+        <input type="radio" id="GG" name="size" value="GG" />
         <label for="GG">GG</label>
       </div>
 
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,46 +1,25 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
+import { useState } from "react";
 import "./Categories.css";
-import { Navigation } from "swiper";
 
 export const Categories = () => {
+  const [category] = useState([
+    { categoryName: "Jeans" },
+    { categoryName: "Camisas" },
+    { categoryName: "Suéter" },
+    { categoryName: "Calças Moletom" },
+    { categoryName: "Camisas Moletom" },
+  ]);
+
   return (
-    <>
-      <h2 id="title-categories">Categorias</h2>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={35}
-        slidesPerGroup={1}
-        loop={true}
-        loopFillGroupWithBlank={true}
-        pagination={false}
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide >
-          <img src="/img/roupas_sem_fundo/jeans1.png" />
-          <a href="#" className="name-categories">Jeans</a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/img/roupas_sem_fundo/shirt2.png" />
-          <a href="#" className="name-categories">Camisas</a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/img/roupas_sem_fundo/sweater2.png"/>
-          <a href="#" className="name-categories">Suéter</a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/img/roupas_sem_fundo/sweatpants1.png" />
-          <a href="#" className="name-categories">Calças Moletom</a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/img/roupas_sem_fundo/sweatshirt3.png" />
-          <a href="#" className="name-categories">Camisas Moletom</a>
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <div>
+      <ul className="categories">
+        {category.map((category) => (
+          <li>
+            <a href="#">{category.categoryName}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
