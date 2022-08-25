42231ad4984e26b8195db7d79b5df8bc38e68861
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-25 16:19:26 -0300

    changes
    Co-authored-by: Victor Emanuel <vitu2@users.noreply.github.com>
    Co-authored-by: Daniel Lucas <daniellucas_santos@hotmail.com>
    Co-authored-by: Geovanna Cunha <geovannacas@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Cadastro from "./pages/Cadastro/cadastro";
 import { Produto } from "./pages/Produto/Produto";
+import { ButtonProduct } from "./components/Button-product/Button-product";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -19,6 +20,7 @@ function App() {
 
         <Route path="/Produto/:itemID/:nome" element={<Produto />}></Route>
       </Routes>
+      <ButtonProduct />
     </>
   );
 }
