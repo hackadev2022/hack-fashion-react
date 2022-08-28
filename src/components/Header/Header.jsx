088ae6f2e79d64d810c9e3fced8654f067ff94e6
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-28 13:46:40 -0300

    header agr desce junto com a página

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -54,79 +54,84 @@ export const Header = ({ searchFn, setShowOffersOnly }) => {
   });
 
   return (
-    <header>
-      <MenuNavBar
-        menuWidth={menuWidth}
-        closeMenu={closeMenu}
-        handleType={{
-          handleMoletom,
-          handleCamiseta,
-          handleCalça,
-          handleJeans,
-          handleJaqueta,
-        }}
-      />
-      <div className="header__top container">
-        <div className="header__imgs">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "activeBtn" : "activeBtn")}
-          >
-            <img
-              className="header__img-logo img-logo"
-              src="/img/logo/logo-img.png"
-              alt="logo da Hack Fashion"
-            />
-            <img
-              className="header__img-name img-name header__img--display-none"
-              src="/img/logo/logo-name.png"
-              alt="Hack Fashion"
-            />
-          </NavLink>
-        </div>
-        <nav className="header__nav">
-          <FontAwesomeIcon icon={faLocationDot} />
-          <NavLink
-            to="/Login"
-            className={({ isActive }) =>
-              isActive ? "activeUser" : "activeUser"
-            }
-          >
-            <FontAwesomeIcon icon={faUser} />
-          </NavLink>
-          <NavLink to="/Carrinho">
-            <FontAwesomeIcon icon={faCartShopping} />
-          </NavLink>
+    <>
+      <header>
+        <MenuNavBar
+          menuWidth={menuWidth}
+          closeMenu={closeMenu}
+          handleType={{
+            handleMoletom,
+            handleCamiseta,
+            handleCalça,
+            handleJeans,
+            handleJaqueta,
+          }}
+        />
+        <div className="header__top container">
+          <div className="header__imgs">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "activeBtn" : "activeBtn"
+              }
+            >
+              <img
+                className="header__img-logo img-logo"
+                src="/img/logo/logo-img.png"
+                alt="logo da Hack Fashion"
+              />
+              <img
+                className="header__img-name img-name header__img--display-none"
+                src="/img/logo/logo-name.png"
+                alt="Hack Fashion"
+              />
+            </NavLink>
+          </div>
+          <nav className="header__nav">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <NavLink
+              to="/Login"
+              className={({ isActive }) =>
+                isActive ? "activeUser" : "activeUser"
+              }
+            >
+              <FontAwesomeIcon icon={faUser} />
+            </NavLink>
+            <NavLink to="/Carrinho">
+              <FontAwesomeIcon icon={faCartShopping} />
+            </NavLink>
 
-          <FontAwesomeIcon icon={faBars} onClick={openMenu} />
-        </nav>
-      </div>
-      <div className="header__bottom container header__bottom--display-none">
-        <div className="header__search ">
-          <input
-            id="header__input-text"
-            type="text"
-            name="search"
-            placeholder="Pesquise"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <i
-            id="SearchEnter"
-            className="fa-solid fa-magnifying-glass"
-            onClick={handleSearch}
-          ></i>
+            <FontAwesomeIcon icon={faBars} onClick={openMenu} />
+          </nav>
         </div>
-        <nav className="header__categories">
-          <ul>
-            <li onClick={handleMoletom}>Moletom</li>
-            <li onClick={handleCamiseta}>Camiseta</li>
-            <li onClick={handleCalça}>Calça</li>
-            <li onClick={handleJeans}>Jeans</li>
-            <li onClick={handleJaqueta}>Jaqueta</li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+        <div className="header__bottom container header__bottom--display-none">
+          <div className="header__search ">
+            <input
+              id="header__input-text"
+              type="text"
+              name="search"
+              placeholder="Pesquise"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <i
+              id="SearchEnter"
+              className="fa-solid fa-magnifying-glass"
+              onClick={handleSearch}
+            ></i>
+          </div>
+          <nav className="header__categories">
+            <ul>
+              <li onClick={handleMoletom}>Moletom</li>
+              <li onClick={handleCamiseta}>Camiseta</li>
+              <li onClick={handleCalça}>Calça</li>
+              <li onClick={handleJeans}>Jeans</li>
+              <li onClick={handleJaqueta}>Jaqueta</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="header-height"></div>
+    </>
   );
 };
