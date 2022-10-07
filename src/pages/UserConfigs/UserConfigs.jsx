164b3d60c3e7565805680fa7b5a0502661d5a3a8
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-10-07 15:24:31 -0300

    Add função para deslogar e opção para logar se não estiver logado

--- src/pages/UserConfigs/UserConfigs.jsx
+++ src/pages/UserConfigs/UserConfigs.jsx
@@ -1,9 +1,22 @@
 import { NavLink } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
 import "./UserConfigs.css";
 
 export const UserConfigs = ({ customerData, setCustomerData }) => {
   console.log("customerData");
   console.log(customerData);
+
+  const fnDeslogar = () => {
+    setCustomerData([
+      {
+        customer_id: "",
+        name: "",
+        phone: "",
+        loged: false,
+      },
+    ]);
+    localStorage.removeItem("customerData");
+  };
   return (
     <section className="section__userConfigs">
       {customerData[0].loged !== true && (
@@ -11,7 +24,12 @@ export const UserConfigs = ({ customerData, setCustomerData }) => {
           <h1>faça o login clicando AQUI</h1>
         </NavLink>
       )}
-      {customerData[0].loged === true && <h1>você está logado</h1>}
+      {customerData[0].loged === true && (
+        <>
+          <h1>você está logado</h1>
+          <Button txt={"DESLOGAR"} fn={fnDeslogar}></Button>
+        </>
+      )}
     </section>
   );
 };
